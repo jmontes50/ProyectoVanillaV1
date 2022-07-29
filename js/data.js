0409afa02391430b0fa149f43af88bd1b00bb82f
Osmar Montesinos <osmarmontes50@gmail.com>
2022-07-29 10:19:14 -0500

    Primer Commit

---
+++ js/data.js
@@ -0,0 +1,53 @@
+const data = [
+    {
+        id: 1,
+        nombre: "Crema de Verduras",
+        descripcion: "Suave crema preparada con una variedad de verduras estacionales",
+        precio: 12.0,
+        stock: 10,
+        imagen: "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80z",
+    },
+    {
+        id: 2,
+        nombre: "Albondigas con salsa Barbeque",
+        descripcion:
+            "Albondigas de carne de res condimentandas con finas hierbas acompañadas con Salsa Bbq y espinacas",
+        precio: 18.0,
+        stock: 8,
+        imagen: "https://images.unsplash.com/photo-1529042410759-befb1204b468?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80",
+    },
+    {
+        id: 3,
+        nombre: "Hamburguesa Royal",
+        descripcion: "Carne, Queso, Huevo y tomate, todo envuelto con pan",
+        precio: 11.0,
+        stock: 14,
+        imagen: "https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    },
+    {
+        id: 4,
+        nombre: "Pizza de la casa",
+        descripcion: "Pizza con recetea original de la casa",
+        precio: 14.0,
+        stock: 7,
+        imagen: "https://images.unsplash.com/photo-1458642849426-cfb724f15ef7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
+    },
+    {
+        id: 5,
+        nombre: "Ceviche de la casa",
+        descripcion: "Plato Bandera Peruano acompañado con bebida a elección",
+        precio: 20.0,
+        stock: 10,
+        imagen: "https://images.unsplash.com/photo-1535399831218-d5bd36d1a6b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    },
+    {
+        id: 6,
+        nombre: "Ramen Fusión",
+        descripcion: "Ramen preparado con ingredientes Peruanos",
+        precio: 19.0,
+        stock: 6,
+        imagen: "https://images.unsplash.com/photo-1614563637806-1d0e645e0940?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+    },
+];
+
+export default data;
